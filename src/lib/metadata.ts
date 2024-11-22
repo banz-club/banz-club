24139efa6b241b72c91d3875460006db3a6a6cc6
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-11-22 02:02:33 +0100

    Added metadata lib and og.png to /public

---
+++ src/lib/metadata.ts
@@ -0,0 +1,32 @@
+import type { Metadata } from "next/types";
+
+export function createMetadata(override: Metadata): Metadata {
+  return {
+    ...override,
+    openGraph: {
+      title: override.title ?? undefined,
+      description: override.description ?? undefined,
+      url: "https://banz.club",
+      images: "https://banz.club/og.png",
+      siteName: "banz.club",
+      ...override.openGraph,
+    },
+    twitter: {
+      card: "summary_large_image",
+      creator: "@ogeperc",
+      title: override.title ?? undefined,
+      description: override.description ?? undefined,
+      images: "https://banz.club/og.png",
+      ...override.twitter,
+    },
+  };
+}
+
+export const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? new URL("http://localhost:3000")
+    : new URL(`https://${process.env.VERCEL_URL!}`);
+
+export const githubProfileUrl = "https://github.com/kWAYTV";
+export const githubRepoUrl =
+  "https://github.com/kWAYTV/hypixel-bans-tracker-site";
