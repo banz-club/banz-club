7bcaf748c4237899930e95a50f0be8a7d9116aa9
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-11-29 06:59:48 +0100

    Using axios instead of fetch

--- src/data-access/bans.ts
+++ src/data-access/bans.ts
@@ -1,11 +1,11 @@
+import axios from "axios";
 import { type BanStats, BanStatsSchema } from "@/interfaces/bans";
 
 export async function fetchBanStats(): Promise<BanStats> {
   try {
-    const res = await fetch(
+    const { data } = await axios.get(
       "https://api.plancke.io/hypixel/v1/punishmentStats"
     );
-    const data = await res.json();
     return BanStatsSchema.parse(data);
   } catch {
     return BanStatsSchema.parse({});
