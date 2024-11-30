ffff41d7caeef974be90466efe1149545df189dd
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-11-30 09:40:04 +0100

    Fixing linter issues

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -5,7 +5,9 @@ import { ThemeProvider } from 'next-themes';
 import { useState } from 'react';
 import { Toaster } from 'sonner';
 
+// !! We could perfectly not have a setter here and forget about this linter line but it's supposedly a code smell
 export function Providers({ children }: { children: React.ReactNode }) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [queryClient, _setQueryClient] = useState(() => new QueryClient());
 
   return (
